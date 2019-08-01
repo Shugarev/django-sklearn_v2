02db0bc70151bbd7988890d19455059c3f051bb0
sh <shugarev74@gmail.com>
2019-08-01 09:39:28 +0300

    start

---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+запуск проекта
+python manage.py runserver
+
+миграция
+python manage.py makemigrations datalab
+python manage.py migrate
+
+
+create DATABASE datalab;
+FLUSH PRIVILEGES;
+SET GLOBAL validate_password_policy = 0;
+CREATE USER 'datalab'@'localhost' IDENTIFIED BY 'Datalab1414';
+GRANT ALL PRIVILEGES ON datalab.* TO 'datalab'@'localhost' IDENTIFIED BY 'Datalab1414';
+FLUSH PRIVILEGES;
+
+
+удалить миграцию
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+
+django.core.exceptions.ImproperlyConfigured: Error loading MySQLdb module.
+Did you install mysqlclient?
+создать и удалить пользователя. Перезапустить проект. Попробовать доставить пакет а потом удалить. Ошибка уйдет.
+
+
+
---
+++ datalab/models.py
@@ -0,0 +1,107 @@
+import os
+from django.db import models
+from django.forms import ModelForm
+from django_mysql.models import JSONField
+from sklearn_django.settings import BASE_DIR
+
+class ExtraModel(models.Model):
+    def is_deletable(self):
+        # get all the related object
+        for rel in self._meta.get_fields():
+            try:
+                # check if there is a relationship with at least one related object
+                related = rel.related_model.objects.filter(**{rel.field.name: self})
+                if related.exists():
+                    # if there is return a Tuple of flag = False the related_model object
+                    error_text = 'This {} has related to: {} ' . format(str(self), str(related[0]))
+                    raise IOError(error_text)
+            except AttributeError:  # an attribute error for field occurs when checking for AutoField
+                pass  # just pass as we dont need to check for AutoField
+
+        return True, None
+
+
+    class Meta:
+        abstract = True
+
+class DataSet(ExtraModel):
+    dataset = models.FileField(upload_to="datasets/")
+    upload_date = models.DateTimeField(auto_now_add =True)
+
+    def delete(self, *args, **kwargs):
+        is_deletable, related = self.is_deletable()
+        if is_deletable:
+            self.dataset.delete()
+            super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return  self.dataset.name
+
+# FileUpload form class.
+class UploadDataSet(ModelForm):
+    class Meta:
+        model = DataSet
+        fields = ('dataset',)
+
+
+class Algorithm(models.Model):
+    algorithm_name = models.CharField(max_length=30,  blank=False)
+    algorithm_short_name = models.CharField(max_length=8,  blank=False)
+    algorithm_config = JSONField()
+
+
+class Profile(ExtraModel):
+    teach = models.ForeignKey(DataSet, on_delete=models.PROTECT)
+    algorithm = models.ForeignKey(Algorithm, on_delete=models.PROTECT)
+    created_date = models.DateTimeField(auto_now_add=True)
+    feature_importance = models.CharField(max_length=64, blank=False)
+    model_name = models.CharField(max_length=30, blank=False)
+    factors = JSONField()
+    profile_config = JSONField()
+
+    def get_drop_columns(self):
+        drop_columns = []
+        for k,v in self.factors.items():
+            if v == 0 or k == 'status':
+                drop_columns.append(k)
+        return drop_columns
+
+    def get_factor_list(self):
+        return [k for k,v in self.factors.items() if v == 1]
+
+    def get_algorithm_params(self):
+        return self.profile_config
+
+
+    def delete(self, *args, **kwargs):
+        is_deletable, related = self.is_deletable()
+        if is_deletable:
+            model_path = "{}/media/{}" . format(BASE_DIR, self.model_name)
+            feature_path = "{}/media/{}" . format(BASE_DIR, self.feature_importance)
+            super().delete(*args, **kwargs)
+            if os.path.isfile(model_path):
+                os.remove(model_path)
+            if os.path.isfile(feature_path):
+                os.remove(feature_path)
+
+    def __str__(self):
+        return ' Model: ' + self.model_name
+
+
+class Experiment(ExtraModel):
+    test = models.ForeignKey(DataSet, on_delete=models.PROTECT)
+    model = models.ForeignKey(Profile, on_delete=models.PROTECT)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    experiment_name = models.CharField(max_length=30, blank=False)
+
+    def delete(self, *args, **kwargs):
+        is_deletable, related = self.is_deletable()
+        if is_deletable:
+            experiment_path = "{}/media/{}" . format(BASE_DIR, self.experiment_name)
+            super().delete(*args, **kwargs)
+            if os.path.isfile(experiment_path):
+                os.remove(experiment_path)
+
+    def __str__(self):
+        return 'Experiment: ' + self.experiment_name
