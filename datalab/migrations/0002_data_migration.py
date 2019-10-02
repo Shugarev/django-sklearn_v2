f9c8dcda5319f68eab8df50cb209f4a8d5027b3c
sh <shugarev74@gmail.com>
2019-10-02 15:26:23 +0300

    установили правильные default параметры для алгоритмов.

--- datalab/migrations/0002_data_migration.py
+++ datalab/migrations/0002_data_migration.py
@@ -3,21 +3,21 @@ from datalab.models import Algorithm
 
 
 def populate_algorithms(*args):
-    default_algorithm_params = {'learning_rate': 0.2, 'max_depth': 2, 'n_estimators': 60, 'random_state': 123}
+    default_algorithm_params = {'learning_rate': 0.1, 'max_depth': 3, 'n_estimators': 100, 'random_state': 123}
     algorithm_params_range = {'learning_rate': [0.2, 0.5, 0.7, 1, 1.5], 'max_depth': [2, 3, 5, 7],
                               'n_estimators': [60, 80, 100, 120, 140], 'random_state': [123]}
     algorithm = Algorithm(algorithm_name='xgboost', default_algorithm_params=default_algorithm_params,
                           algorithm_params_range=algorithm_params_range)
     algorithm.save()
 
-    default_algorithm_params = {'learning_rate': 0.2, 'n_estimators': 60, 'random_state': 123}
+    default_algorithm_params = {'learning_rate': 1.0, 'n_estimators': 50, 'random_state': 123}
     algorithm_params_range = {'learning_rate': [0.2, 0.5, 0.7, 1, 1.5], 'n_estimators': [60, 80, 100, 120, 140],
                               'random_state': [123]}
     algorithm = Algorithm(algorithm_name='adaboost', default_algorithm_params=default_algorithm_params,
                           algorithm_params_range=algorithm_params_range)
     algorithm.save()
 
-    default_algorithm_params = {'learning_rate': 0.2, 'max_depth': 2, 'n_estimators': 120, 'random_state': 123}
+    default_algorithm_params = {'learning_rate': 0.1, 'max_depth': 3, 'n_estimators': 100, 'random_state': 123}
     algorithm_params_range = {'learning_rate': [0.2, 0.5, 0.7, 1, 1.5], 'max_depth': 2,
                               'n_estimators': [60, 80, 100, 120, 140], 'random_state': [123]}
     algorithm = Algorithm(algorithm_name='gradientboost', default_algorithm_params=default_algorithm_params,
@@ -31,7 +31,7 @@ def populate_algorithms(*args):
     algorithm.save()
 
     default_algorithm_params = {'random_state': 123}
-    algorithm_params_range = {}
+    algorithm_params_range = {'max_iter': 100, 'penalty': 'l2'}
     algorithm = Algorithm(algorithm_name='logregression', default_algorithm_params=default_algorithm_params,
                           algorithm_params_range=algorithm_params_range)
     algorithm.save()
